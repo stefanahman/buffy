3cf264ae375a337f1ea63a78ba5f4910e5457aa7
Stefan Åhman <stefan.ahman@gmail.com>
2018-11-11 10:57:28 +0100

    Updates

---
+++ precache-manifest.11e75498847073fb4719ad758ab22e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/buffy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af59a92a2b82a42bc497",
+    "url": "/buffy/static/js/runtime~main.af59a92a.js"
+  },
+  {
+    "revision": "9899a28ccd300329addc",
+    "url": "/buffy/static/js/main.9899a28c.chunk.js"
+  },
+  {
+    "revision": "49a5790dabf855979d53",
+    "url": "/buffy/static/js/1.49a5790d.chunk.js"
+  },
+  {
+    "revision": "9899a28ccd300329addc",
+    "url": "/buffy/static/css/main.20685434.chunk.css"
+  },
+  {
+    "revision": "72089b063bc134fe7145da785a4b91dd",
+    "url": "/buffy/index.html"
+  }
+];
